f3e85da2886b51389e75f47f73eb9ad1dea20888
DZM77 <32109758+DZM77@users.noreply.github.com>
2024-08-21 10:19:51 +0200

    Add Studenrt

---
+++ University.API/Models/Entities/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace University.API.Models.Entities
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName => $"{FirstName} {LastName}";
+    }
+}
