32d53047267025cb52c68555d4448b1148b179aa
DZM77 <32109758+DZM77@users.noreply.github.com>
2024-08-22 14:55:03 +0200

    Add enrollments to course

--- University.API/Models/Entities/Course.cs
+++ University.API/Models/Entities/Course.cs
@@ -7,6 +7,7 @@ namespace University.API.Models.Entities
         public int Id { get; set; }
         public string Title { get; set; }
 
+        public ICollection<Enrollment> Enrollment { get; set; }
         public ICollection<Student> Students { get; set; }
     }
 }
